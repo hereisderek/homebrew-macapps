1214083188daa8d1cf49edf3419a86428bcea13a
Derek <1and1get2@gmail.com>
2026-01-14 14:05:26 +1300

    Update apps: DisplayBuddy, Proxyman (Bump to v0.4.1)

--- Casks/display-buddy.rb
+++ Casks/display-buddy.rb
@@ -2,7 +2,7 @@ cask "display-buddy" do
   version "3.1.0"
   sha256 "d9fcb14747dc6fca974dcd914901397b7dbf65dab29cf80571801eb5c2d9a2e2"
 
-  url "https://github.com/hereisderek/homebrew-macapps/releases/download/v0.4.0/DisplayBuddy-3.1.0.dmg"
+  url "https://github.com/hereisderek/homebrew-macapps/releases/download/v0.4.1/DisplayBuddy-3.1.0.dmg"
   name "DisplayBuddy"
   desc "DisplayBuddy App"
   homepage "https://github.com/hereisderek/homebrew-macapps"
--- Casks/proxyman.rb
+++ Casks/proxyman.rb
@@ -2,7 +2,7 @@ cask "proxyman" do
   version "6.3.0"
   sha256 "7b9efa229588f86e2f999e278e3374d8e55d78735aa405dfa53f3d3da4cf093d"
 
-  url "https://github.com/hereisderek/homebrew-macapps/releases/download/v0.4.0/Proxyman-6.3.0.dmg"
+  url "https://github.com/hereisderek/homebrew-macapps/releases/download/v0.4.1/Proxyman-6.3.0.dmg"
   name "Proxyman"
   desc "Proxyman App"
   homepage "https://github.com/hereisderek/homebrew-macapps"
