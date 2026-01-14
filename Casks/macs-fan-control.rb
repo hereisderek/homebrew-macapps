a646e9b3de22283ab89fab03830160dc13812ac9
Derek <1and1get2@gmail.com>
2026-01-14 14:36:13 +1300

    Update apps: MacsFanControl (Bump to v0.6.1)

--- Casks/macs-fan-control.rb
+++ Casks/macs-fan-control.rb
@@ -2,12 +2,12 @@ cask "macs-fan-control" do
   version "1.5.20"
   sha256 "1e56155448791b3db2fbaf9ca6569a1f53f67908da6b5772d4b7f5d2d83a3169"
 
-  url "https://github.com/hereisderek/homebrew-macapps/releases/download/v0.6.0/MacsFanControl-1.5.20.dmg"
+  url "https://github.com/hereisderek/homebrew-macapps/releases/download/v0.6.1/MacsFanControl-1.5.20.dmg"
   name "MacsFanControl"
   desc "MacsFanControl App"
   homepage "https://github.com/hereisderek/homebrew-macapps"
 
-  app "MacsFanControl.app"
+  app "Macs Fan Control.app"
   postflight do
     system_command "/usr/bin/xattr",
                    args: ["-d", "com.apple.quarantine", "#{staged_path}/MacsFanControl.app"],
