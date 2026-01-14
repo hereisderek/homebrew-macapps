3913d2d3bf4a6683552f7fe76a60b4a93267c840
Derek <1and1get2@gmail.com>
2026-01-14 13:30:22 +1300

    Update apps: DaisyDisk (Bump to v0.2.1)

--- Casks/daisy-disk.rb
+++ Casks/daisy-disk.rb
@@ -2,7 +2,7 @@ cask "daisy-disk" do
   version "4.33.2"
   sha256 "acee0b30898c29a877c46ca77eb67c8fd5b312389f8b2415ab6d565fd6c7db88"
 
-  url "https://github.com/hereisderek/homebrew-macapps/releases/download/v0.2.0/DaisyDisk-4.33.2.dmg"
+  url "https://github.com/hereisderek/homebrew-macapps/releases/download/v0.2.1/DaisyDisk-4.33.2.dmg"
   name "DaisyDisk"
   desc "DaisyDisk App"
   homepage "https://github.com/hereisderek/homebrew-macapps"
